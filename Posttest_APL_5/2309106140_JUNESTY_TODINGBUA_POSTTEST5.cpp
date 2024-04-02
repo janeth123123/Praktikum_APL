b8d20bb30530f15a09a0d37dd5059a4b12909374
janeth123123 <161612552+janeth123123@users.noreply.github.com>
2024-04-03 00:08:49 +0800

    Add files via upload

---
+++ Posttest_APL_5/2309106140_JUNESTY_TODINGBUA_POSTTEST5.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Flower {
+    string name;
+    int price;
+};
+
+Flower flowers[10];
+int flowerCount = 0;
+int login = 0;
+
+bool loginProcess() {
+    cout << " (Masukkan Nama): ";
+    string nama_user;
+    cin >> nama_user;
+    cout << "(Masukkan Nim): ";
+    int nim_user;
+    cin >> nim_user;
+
+    if (nama_user == "Junesty" && nim_user == 140) {
+        cout << "Login berhasil!\n";
+        return true;
+    } else {
+        cout << "Login gagal. Silakan coba lagi.\n";
+        login++;
+        if (login >= 3) {
+            cout << "Anda telah mencapai batas maksimum percobaan login. Program berhenti.\n";
+            return false;
+        }
+        return false;
+    }
+}
+
+void displayMenu() {
+    cout << "\nSelamat Datang di Toko Bunga!\n";
+    cout << "1. Tambah Bunga\n";
+    cout << "2. Hapus Bunga\n";
+    cout << "3. Ubah Bunga\n";
+    cout << "4. Lihat Jenis Bunga yang Tersedia\n";
+    cout << "5. Keluar\n";
+    cout << "Pilih menu: ";
+}
+
+void addFlower(Flower* flower) {
+    if (flowerCount < 10) {
+        cout << "Masukkan Nama Bunga: ";
+        cin >> flower->name;
+        cout << "Masukkan Harga Bunga: ";
+        cin >> flower->price;
+        cout << "Bunga Berhasil Ditambahkan!\n";
+        flowerCount++;
+    } else {
+        cout << "Maaf, tidak bisa menambahkan bunga. Sudah mencapai batas maksimum.\n";
+    }
+}
+
+void deleteFlower(int* index) {
+    if (flowerCount > 0) {
+        if (*index >= 0 && *index < flowerCount) {
+
+            for (int i = *index; i < flowerCount - 1; i++) {
+                flowers[i] = flowers[i + 1];
+            }
+            flowerCount--;
+            cout << "Bunga Berhasil dihapus!\n";
+        } else {
+            cout << "Indeks tidak valid!\n";
+        }
+    } else {
+        cout << "Tidak ada bunga untuk dihapus.\n";
+    }
+}
+
+void updateFlower(int* index, Flower* flower) {
+    if (flowerCount > 0) {
+        if (*index >= 0 && *index < flowerCount) {
+            cout << "Masukkan Nama Bunga Baru: ";
+            cin >> flower->name;
+            cout << "Masukkan Harga Bunga Baru: ";
+            cin >> flower->price;
+            cout << "Bunga Berhasil Diubah!\n";
+        } else {
+            cout << "Indeks tidak valid!\n";
+        }
+    } else {
+        cout << "Tidak ada bunga untuk diubah.\n";
+    }
+}
+
+void displayFlowers() {
+    if (flowerCount > 0) {
+        cout << "Daftar Bunga:\n";
+        for (int i = 0; i < flowerCount; i++) {
+            cout << "Indeks: " << i << endl;
+            cout << "Nama Bunga: " << flowers[i].name << "\n";
+            cout << "Harga Bunga: " << flowers[i].price << "\n";
+        }
+    } else {
+        cout << "Tidak ada bunga yang tersedia.\n";
+    }
+}
+
+void recursiveLogin() {
+    if (!loginProcess()) {
+        recursiveLogin();
+    }
+}
+
+int main() {
+    recursiveLogin();
+
+    int pilihan;
+    while (true) {
+        displayMenu();
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1:
+                addFlower(&flowers[flowerCount]);
+                break;
+
+            case 2: {
+                int index;
+                cout << "Masukkan Indeks Bunga Yang Ingin dihapus (0 - " << flowerCount - 1 << "): ";
+                cin >> index;
+                deleteFlower(&index);
+                break;
+            }
+
+            case 3: {
+                int index;
+                cout << "Masukkan Indeks Bunga Yang Ingin diubah (0 - " << flowerCount - 1 << "): ";
+                cin >> index;
+                updateFlower(&index, &flowers[index]);
+                break;
+            }
+
+            case 4:
+                displayFlowers();
+                break;
+
+            case 5:
+                cout << "Sampai Jumpa!\n";
+                return 0;
+                break;
+
+            default:
+                cout << "Pilihan tidak valid!\n";
+                break;
+        }
+    }
+
+    return 0;
+}
